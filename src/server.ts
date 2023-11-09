226a716bfd964a2adac5bd4c713c8364513b6a56
Alon Vaisgur <37458479+Romeore@users.noreply.github.com>
2023-11-09 11:49:09 +0200

    [Feature] GET method seen devices only

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 // src/server.ts
 
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
@@ -13,31 +13,49 @@ interface Device {
     latitude: number;
     longitude: number;
   };
-  status: 'Seen' | 'Missing' | 'Repair';
+  status: "Seen" | "Missing" | "Repair";
 }
 
-  const devices: Device[] = [
-    {
-      id: "1",
-      name: "Device 1",
-      location: {
-        latitude: 51.1614,
-        longitude: 4.9654,
-      },
-      status: "Seen",
+const devices: Device[] = [
+  {
+    id: "1",
+    name: "Device 1",
+    location: {
+      latitude: 51.1614,
+      longitude: 4.9654,
     },
-    {
-      id: "2",
-      name: "Device 2",
-      location: {
-        latitude: 51.1624,
-        longitude: 4.9644,
-      },
-      status: "Seen",
+    status: "Seen",
+  },
+  {
+    id: "2",
+    name: "Device 2",
+    location: {
+      latitude: 51.1624,
+      longitude: 4.9644,
     },
-  ];
+    status: "Seen",
+  },
+  {
+    id: "3",
+    name: "Device 3",
+    location: { latitude: -1, longitude: -1 },
+    status: "Missing",
+  },
+  {
+    id: "4",
+    name: "Device 4",
+    location: { latitude: -1, longitude: -1 },
+    status: "Repair",
+  },
+];
 
-app.get('/devices', (req: Request, res: Response) => {
+app.get("/seenDevices", (req: Request, res: Response) => {
+  const seenDevices = devices.filter(device => device.status === "Seen");
+
+  res.json(seenDevices);
+});
+
+app.get("/devices", (req: Request, res: Response) => {
   res.json(devices);
 });
 
